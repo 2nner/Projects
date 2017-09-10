28c7b79bbd6bf9fd0b4593d2f7a4bb32a84cd14a
2nner <khsbs001@naver.com>
2017-09-11 00:06:44 +0900

    frontcam

--- app/src/main/java/com/example/pc/forwardcam_new/MainActivity.java
+++ app/src/main/java/com/example/pc/forwardcam_new/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         if(pref.getBoolean(Constants.IS_LOGGED_IN,false)) {
             fragment = new FirstFragment();
-            Log.e("asdf", "First");
         } else {
-
             fragment = new LoginFragment();
-            Log.e("asdf", "LOG");
         }
 
         FragmentTransaction ft = getFragmentManager().beginTransaction();
