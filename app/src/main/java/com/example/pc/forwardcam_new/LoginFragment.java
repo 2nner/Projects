38fa769ef9c7d4dfe7de103eaf0e6ae93d83d3fa
2nner <khsbs001@naver.com>
2017-09-11 23:27:36 +0900

    frontcam

--- app/src/main/java/com/example/pc/forwardcam_new/LoginFragment.java
+++ app/src/main/java/com/example/pc/forwardcam_new/LoginFragment.java
@@ -4,6 +4,8 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class LoginFragment extends Fragment {
@@ -71,16 +78,64 @@ public class LoginFragment extends Fragment {
         goToRegister();
     }
 
-    private void loginProcess(String eamil, String pasword) {
+    private void loginProcess(String email, String password) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        RequestInterface requestInterface = retrofit.create(RequestInterface.class);
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        ServerRequest request = new ServerRequest();
+        request.setOperation(Constants.LOGIN_OPERATION);
+        request.setUser(user);
+        Call<ServerResponse> response = requestInterface.operation(request);
+
+        response.enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, retrofit2.Response<ServerResponse> response) {
+
+                ServerResponse resp = response.body();
+                Snackbar.make(getView(), resp.getMessage(), Snackbar.LENGTH_LONG).show();
+
+                if(resp.getResult().equals(Constants.SUCCESS)){
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putBoolean(Constants.IS_LOGGED_IN,true);
+                    editor.putString(Constants.EMAIL,resp.getUser().getEmail());
+                    editor.putString(Constants.LASTNAME,resp.getUser().getLastname());
+                    editor.putString(Constants.FIRTSNAME,resp.getUser().getFirstname());
+                    editor.apply();
+                    goToFirst();
+                }
+                progress.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+
+                progress.setVisibility(View.INVISIBLE);
+                Log.d(Constants.TAG,"failed");
+                Snackbar.make(getView(), t.getLocalizedMessage(), Snackbar.LENGTH_LONG).show();
+
+            }
+        });
     }
 
-    private void goToRegister(){
-        Fragment reg = new RegisterFragment();
+    private void goToRegister() {
+        Fragment fragment = new RegisterFragment();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_frame,reg);
+        ft.replace(R.id.fragment_frame,fragment);
         ft.commit();
     }
 
+    private void goToFirst() {
+        Fragment fragment = new FirstFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame,fragment);
+        ft.commit();
+    }
 
 }
