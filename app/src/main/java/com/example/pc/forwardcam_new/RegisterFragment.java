43f5347623f51f1ae9c0f79363954afba1069c56
2nner <khsbs001@naver.com>
2017-09-13 14:53:52 +0900

    NullPointerException

--- app/src/main/java/com/example/pc/forwardcam_new/RegisterFragment.java
+++ app/src/main/java/com/example/pc/forwardcam_new/RegisterFragment.java
@@ -29,8 +29,8 @@ public class RegisterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+        context = getActivity();
         initViews(view);
-
         return view;
     }
 
