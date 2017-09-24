bab3157a0f7016fc8ac9c2b044a9b52fe8094333
Taehyuny <hyuny6012@naver.com>
2017-09-24 14:59:11 +0900

    9-24 수정본

--- app/src/main/java/com/example/pc/forwardcam_new/BluetoothFragment.java
+++ app/src/main/java/com/example/pc/forwardcam_new/BluetoothFragment.java
@@ -1,40 +1,38 @@
 package com.example.pc.forwardcam_new;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 
-
 public class BluetoothFragment extends Fragment{
+
+    Button btn_goTobluetooth;
     public  BluetoothFragment(){}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.fragment_bluetooth, container, false);
+        View view = inflater.inflate(R.layout.fragment_bluetooth,container,false);
+        initView(view);
+        return view;
+    }
 
-        // String[] values = {"Test1","Test2","Test3","Test4"};
-        // ArrayAdapter<String> adapter = new ArrayAdapter (this, R.layout.list_textview, values);
-        /*ArrayAdapter<String> adapter;
-        ArrayList<String> al = new ArrayList<String>();
-        al.add("망고");
-        al.add("수박");
-        al.add("오렌지");
-        al.add("귤");
-        adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),list_textview,al );
-
-        ListView list = (ListView) getActivity().findViewById(R.id.message_listview);
-        list.setAdapter(adapter);*/
-
-        return layout;
+    private void initView(View view) {
+        btn_goTobluetooth = (Button) view.findViewById(R.id.btn_goTobluetooth);
+        btn_goTobluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(BluetoothFragment.this.getActivity(), BluetoothActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
