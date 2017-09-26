7106b1b77005fddd480fe34680af1e2de63e1868
Taehyuny <hyuny6012@naver.com>
2017-09-26 17:36:17 +0900

    9.26 태현이가 수정한거 '_'

--- app/src/main/java/com/example/pc/forwardcam_new/UserFragment.java
+++ app/src/main/java/com/example/pc/forwardcam_new/UserFragment.java
@@ -27,8 +27,7 @@ public class UserFragment extends Fragment {
     SharedPreferences pref;
     ImageButton logout;
     Context context;
-    ImageButton change_pw;
-    //Activity root = getActivity();
+    ImageButton change_pw; // 태현이가 추가한 버튼(비밀번호 변경)
 
     public  UserFragment(){}
 
@@ -45,7 +44,7 @@ public class UserFragment extends Fragment {
         return layout;
     }
 
-    void show()
+    void show()  // 태현이가 추가한 메소드 (Alertdialog 에 사용됨)
     {
         final EditText edittext = new EditText(this.getActivity());
         InputFilter[] FilterArray = new InputFilter[1];
@@ -57,7 +56,7 @@ public class UserFragment extends Fragment {
         builder.setMessage("바꿀 비밀번호를 입력해주세요");
         builder.setView(edittext);
         builder.setPositiveButton("확인",
-                new DialogInterface.OnClickListener() {
+                new DialogInterface.OnClickListener() { // 확인 버튼 누르면 토스트 처리 해주는 부분, 너가 이 부분 입력받은 비번 실제로 변경되게 해줭
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(getActivity(),"말로만 변경되었습니다" ,Toast.LENGTH_LONG).show();
                     }
@@ -76,10 +75,10 @@ public class UserFragment extends Fragment {
         pref = getActivity().getSharedPreferences("pref", MODE_PRIVATE);
 
         logout = (ImageButton) view.findViewById(R.id.logout);
-        change_pw = (ImageButton) view.findViewById(R.id.btn_user_pw);
+        change_pw = (ImageButton) view.findViewById(R.id.btn_user_pw); // 태현이가 추가함 (용도: Alertdialog)
 
 
-        change_pw.setOnClickListener(new View.OnClickListener() {
+        change_pw.setOnClickListener(new View.OnClickListener() { // 태현이가 추가함 (용도 : Alertdialog)
             @Override
             public void onClick(View v) {
                 show();
