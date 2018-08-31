54838aacb7bc6086405c8f86ef8ab8c973fc15b1
asag0704 <dhdbdnjs58@gmail.com>
2018-09-01 01:49:28 +0900

    add bottomNavigation, viewPager

---
+++ app/src/main/java/com/dev/inner/owntrip/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.dev.inner.owntrip;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/dev/inner/owntrip/MainActivity.java
+++ app/src/main/java/com/dev/inner/owntrip/MainActivity.java
@@ -1,13 +1,93 @@
 package com.dev.inner.owntrip;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager vp_main_viewPager;
+
+    BottomNavigationView bn_main_bottomNavi;
+
+    MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vp_main_viewPager = findViewById(R.id.vp_main_viewPager);
+
+        bn_main_bottomNavi = findViewById(R.id.bn_main_bottomNavi);
+
+        vp_main_viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return new HomeFragment();
+                    case 1:
+                        return null;
+                    case 2:
+                        return null;
+                    case 3:
+                        return null;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        });
+
+        vp_main_viewPager.setOffscreenPageLimit(1);
+
+        vp_main_viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                }
+                bn_main_bottomNavi.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        bn_main_bottomNavi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.item_home:
+                        vp_main_viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.item_second:
+                        vp_main_viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.item_third:
+                        vp_main_viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.item_fourth:
+                        vp_main_viewPager.setCurrentItem(3);
+                }
+                return false;
+            }
+        });
     }
 }
